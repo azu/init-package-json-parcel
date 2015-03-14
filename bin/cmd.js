69a3538ead766f61b7bbab012cdbd85565001c4d
azu <azuciao@gmail.com>
2015-03-14 22:02:24 +0900

    fix(lib): reflect npmrc

--- bin/cmd.js
+++ bin/cmd.js
@@ -4,7 +4,7 @@ var init = require("../lib/init-package-json-parcel");
 init().then(function () {
     process.exit(0);
 }, function (error) {
-    console.error(error);
+    console.error(error.stack);
     process.exit(1);
 });
 process.on("error", function (error) {
